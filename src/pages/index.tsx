3d9fd598a99b630f4ae08ee71cb17c23d5a52a12
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2022-04-14 22:00:56 -0300

    config husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const baba = '321';
   return (
     <div className={styles.container}>
       <Head>
