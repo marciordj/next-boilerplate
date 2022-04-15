e478760bf416cf4afe2f98829ad4626975cb66ba
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2022-04-14 21:58:34 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const baba = '321';
   return (
     <div className={styles.container}>
       <Head>
